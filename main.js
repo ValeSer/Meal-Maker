01f0cdf61d873c5ad804322f77366a14939091f3
Valeria Serci <balerty84@gmail.com>
2022-11-29 17:09:47 +0000

    implement setter and test it

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const menu = {
     _meal: '',
     _price: 0,
-    set meal(mealToCheck) {}
-};
-menu._meal = 'meatballs';
-menu._price = 1000;
-console.log(menu._meal);
-console.log(menu._price);
-
+    set meal(mealToCheck) {
+      if (typeof mealToCheck === 'string') {
+        return this._meal = mealToCheck;
+      }
+    }
+  };
+  
+  menu._meal = 'meatballs';
+  menu._price = 1000;
+  console.log(menu.meal = 'd');
+  console.log(menu._meal );
