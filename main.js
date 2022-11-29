ce90b62fe86d9ea5c69b58fa490220c7acb1b278
Valeria Serci <balerty84@gmail.com>
2022-11-29 16:58:25 +0000

    add a setter for meal

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const menu = {
     _meal: '',
-    _price: 0
+    _price: 0,
+    set meal(mealToCheck) {}
 };
 menu._meal = 'meatballs';
 menu._price = 1000;
 console.log(menu._meal);
 console.log(menu._price);
+
